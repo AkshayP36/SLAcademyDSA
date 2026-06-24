6941520096b227f611715f78cf22728205d55d22
Akshay Patil <54596061+AkshayP36@users.noreply.github.com>
2026-06-24 14:46:54 +0530

    Arrays - carry forward and subarrays

---
+++ dsaNotes/src/main/java/Arrays1/Day32_CarryForwardAndSubarrays/Q1_TotalNumberOfSubarrays.java
@@ -0,0 +1,15 @@
+package Arrays1.Day32_CarryForwardAndSubarrays;
+
+/*
+Given an array of size 21, find the total number of subarrays of the array.
+ */
+public class Q1_TotalNumberOfSubarrays {
+
+    /*
+    formula = n*(n+1)/2
+     */
+
+    public static void main(String[] args) {
+        System.out.println("ans = " + 231);
+    }
+}
